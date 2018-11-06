bd9ee7395e8d9fa6527cf6d920c716f23ddea203
YankunLi <312351771@qq.com>
2018-11-06 20:26:26 +0800

    update

--- interface/interface.go
+++ interface/interface.go
@@ -6,14 +6,18 @@ type Face interface {
 	Show()
 }
 
-type ReadFace struct{}
-type BlackFace struct{}
+type RedFace struct {
+	color string
+}
+type BlackFace struct {
+	color string
+}
 
-func (r ReadFace) Show() {
+func (r *RedFace) Show() {
 	fmt.Println("This face is Red")
 }
 
-func (b BlackFace) Show() {
+func (b *BlackFace) Show() {
 	fmt.Println("This face is Black")
 }
 
@@ -21,11 +25,49 @@ func Smile(face Face) {
 	face.Show()
 }
 
+func GetFace(b bool) Face {
+	if b {
+		return &RedFace{color: "red"}
+	} else {
+		return &BlackFace{color: "black"}
+	}
+}
+
+func describe(t Face) {
+	fmt.Printf("Interface type : %T, value: %v\n", t, t)
+}
+
+func assert(t Face) {
+	//	v := t.(*RedFace)
+	//fmt.Println(v)
+	v, ok := t.(*RedFace)
+	fmt.Println(v, ok)
+}
+
+func findType(i Face) {
+	switch i.(type) {
+	case *RedFace:
+		fmt.Printf("RedFace: %s\n", i.(*RedFace))
+	case *BlackFace:
+		fmt.Printf("BlackFace: %s\n", i.(*BlackFace))
+	default:
+		fmt.Printf("Unknown type\n")
+	}
+}
 func main() {
-	var ff = []Face{ReadFace{}, BlackFace{}}
+	var ff = []Face{&RedFace{}, &BlackFace{}}
 
 	for _, val := range ff {
 		Smile(val)
 	}
+
+	face := GetFace(true)
+	face.Show()
+	describe(face)
+
+	assert(&RedFace{color: "Red"})
+	assert(&BlackFace{color: "Black"})
+
+	findType(face)
 
 }
